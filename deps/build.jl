f490ff98c2aa417237bfa6a279211cab529feabc
Berk Onat <onatberk@macberk-pro.wifi-campus.warwick.ac.uk>
2020-09-28 12:50:09 +0100

    Adding dependent Python package installations.

---
+++ deps/build.jl
@@ -0,0 +1,29 @@
+using PyCall
+
+@info "Building Python packages."
+
+const PACKAGES = ["dscribe","soaplite","amp-atomistics"]
+
+# Use eventual proxy info
+proxy_arg=String[]
+if haskey(ENV, "http_proxy")
+    push!(proxy_arg, "--proxy")
+    push!(proxy_arg, ENV["http_proxy"])
+end
+
+# Import pip
+try
+    pyimport("pip")
+catch
+    # If it is not found, install it
+    println("Pip not found on your sytstem. Downloading it.")
+    get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
+    download("https://bootstrap.pypa.io/get-pip.py", get_pip)
+    run(`$(PyCall.python) $(proxy_arg) $get_pip --user`)
+end
+
+println("Installing required python packages using pip")
+run(`$(PyCall.python) $(proxy_arg) -m pip install --user --upgrade pip setuptools`)
+run(`$(PyCall.python) $(proxy_arg) -m pip install --user $(PACKAGES)`)
+
+
